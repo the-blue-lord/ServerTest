b3a10471b0adfc2a532bac392318b7b935a577a3
Mr. Blue <provaprove3@gmail.com>
2024-09-19 23:12:20 +0200

    File edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as child_process from "child_process";
-import * as os from "os";
+const child_process = require("child_process");
+const os = require("os");
 
 // Get OS information
 const platform = os.platform(); // e.g., 'linux', 'win32', 'darwin'
@@ -7,8 +7,9 @@ const arch = os.arch(); // e.g., 'x64', 'arm', 'ia32'
 
 console.log(`Operating System: ${platform}`);
 console.log(`Architecture: ${arch}`);
-
+/*
 child_process.execFile("./hello.exe", ["manual"], function(err, data) {
     console.log(err);
     console.log(data.toString());
 });
+*/
