70ff664054d456d82874408b2fe3585d6006604d
Mr. Blue <provaprove3@gmail.com>
2024-09-20 15:12:13 +0200

    The JS files runs the 32bit linux binary

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const arch = os.arch(); // e.g., 'x64', 'arm', 'ia32'
 
 console.log(`Operating System: ${platform}`);
 console.log(`Architecture: ${arch}`);
-/*
-child_process.execFile("./hello.exe", ["manual"], function(err, data) {
+
+child_process.execFile("./hello-32bit.out", ["manual"], function(err, data) {
     console.log(err);
     console.log(data.toString());
 });
-*/
