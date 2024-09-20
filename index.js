494235367fa4fc860f447b9b1e7e1313555334b5
Mr. Blue <provaprove3@gmail.com>
2024-09-20 15:36:19 +0200

    Added rewriting of server permissions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const child_process = require("child_process");
 const os = require("os");
+const fs = require("fs");
 
 // Get OS information
 const platform = os.platform(); // e.g., 'linux', 'win32', 'darwin'
@@ -8,7 +9,14 @@ const arch = os.arch(); // e.g., 'x64', 'arm', 'ia32'
 console.log(`Operating System: ${platform}`);
 console.log(`Architecture: ${arch}`);
 
-child_process.execFile("./hello-32bit.out", ["manual"], function(err, data) {
-    console.log(err);
-    console.log(data.toString());
+fs.chmod("./hello-32bit.out", 0o755, (err) => {
+    if(err) {
+        console.error("Error seeting execute permissions", err);
+        return;
+    }
+
+    child_process.execFile("./hello-32bit.out", ["manual"], function(err, data) {
+        console.log(err);
+        console.log(data.toString());
+    });
 });
